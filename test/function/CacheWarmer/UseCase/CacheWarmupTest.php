fb916fadaaf9829949b5a0ce25bca8816608f8dc
Tim Neumann <tn@move-elevator.de>
2017-04-24 17:10:13 +0200

    removed the rest of typo3

--- test/function/CacheWarmer/UseCase/CacheWarmupTest.php
+++ test/function/CacheWarmer/UseCase/CacheWarmupTest.php
@@ -2,8 +2,6 @@
 
 namespace vonZnotz\CacheWarmer\Test\Functional\UseCase;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\IndexedSearch\Indexer;
 use vonZnotz\CacheWarmer\App;
 use PHPUnit\Framework\TestCase;
 
